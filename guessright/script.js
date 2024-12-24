760401792c01db6f176cc580a642df0c7bdac882
Kapil Raut <kapilraut026@gmail.com>
2024-12-24 09:01:09 +0545

    1

--- guessright/script.js
+++ guessright/script.js
@@ -1,28 +1,21 @@
-let boxes = document.getElementById("box");
-let point= document.getElementById("part");
-const container=document.getElementById("container");
 
 
-let number= Math.floor(Math.random()*100 + 1);
+let number = Math.floor(Math.random() * 100 + 1);
 console.log(number);
-container.innerHTML= `Hello ${number}`;
 
-function solve(){
-let no=prompt("Guess the number");
-console.log(no);
-if(no==number){
+
+function solve() {
+  let no = prompt("Guess the number");
+  console.log(no);
+  if (no == number) {
     console.log("You guessed the right number");
-    
-}
-else if(no<number){
-    console.log("The guessed number is less")
-solve();
-}
-else{
+  } else if (no < number) {
+    console.log("The guessed number is less");
+    solve();
+  } else {
     console.log("The guessed number is more");
     solve();
-}
+  }
 }
 
 solve();
-
